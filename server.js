3fcc53e3d9e4ecad5babf9fd7cf325d15b74de20
colemanglisson <coleman@treeoflifefunding.com>
2026-09-04 12:07:03 -0400

    Update REP_MAP with new owner IDs
    Removed some owner IDs and added new ones in the REP_MAP.

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const REP_MAP = {
   '005Hr00000HUOVEIA5': 'Tony M.',
   '005aZ00000AB5GWQA1': 'Rafael R.',
   '005Hr00000HUOUzIAP': 'Benjamin S.',
-  '005Hr00000IEPIBIA5': 'John R.',
-  '005aZ00000WYXOvQAP': 'Miguel A.',
-  '005aZ00000NRfg9QAD': 'Charles F.',
   '005Hr00000IEPI6IAP': 'Richard N.',
   '005aZ00000dBvaqQAC': 'Larissa S.',
   '005aZ00000cOb6zQAC': 'Lorenson J.',
-  '005aZ00000dBvarQAC': 'Jason B.'
+  '005aZ00000dBvarQAC': 'Jason B.',
+  '005aZ00000euJCDQA2': 'Saad H.',
+  '005aZ00000jo2ZXQAY': 'Kwasi M.',
+  '005aZ00000jo2ZVQAY': 'Christian F.'
 };
 
 const OWNER_IDS = Object.keys(REP_MAP);
